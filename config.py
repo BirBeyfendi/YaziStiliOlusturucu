409bc55306240059d50ae3cf9405bcceccb5b77c
shorteg <111714603+shorteg@users.noreply.github.com>
2022-11-11 21:15:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ class Config(object):
       BOT_TOKEN = "5780728163:AAFlrRmr5S53ZYB7Z0-aqElsddEGkPmxSbk",
       API_ID = "14993791",
       API_HASH = "60a2897e2d7de8eefd6cf610085b1f0a",
-      OWNER_ID = ""
+      OWNER_ID = "1720623907"
 
