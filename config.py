f7459659b7d0caefaecb9f6ca603e56f3657145c
shorteg <111714603+shorteg@users.noreply.github.com>
2022-11-13 14:09:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
 
-      BOT_TOKEN = "5780728163:AAFlrRmr5S53ZYB7Z0-aqElsddEGkPmxSbk",
+      BOT_TOKEN = "5780728163:AAEOfxKcPS6zwoJbwq0n972_g440IAXdf7Q",
       API_ID = "14993791",
       API_HASH = "60a2897e2d7de8eefd6cf610085b1f0a",
       OWNER_ID = "1720623907"
